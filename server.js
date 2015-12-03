d09d1360a5238609bf8295d493858e30d6d7a3cd
Lindsay Williams <williamsl25@yahoo.com>
2015-12-03 10:08:17 -0500

    changed http to https

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 3001;
 var app = express();
-var https = require('https').Server(app);
+var http = require('http').Server(app);
 
 app.use(express.static(__dirname + "/app"))
 
